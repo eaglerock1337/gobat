64c3004df5ea33b0776ef1dbaf6a9ff218a0f33a
Peter Marks <peter.marks@gmail.com>
2020-11-20 18:03:38 -0500

    More square writeup

--- internal/board/square.go
+++ internal/board/square.go
@@ -1,27 +1,41 @@
 package board
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Square is a struct for holding a coordinate on the board
 type Square struct {
-	letter int
-	number int
+	Letter int
+	Number int
 }
 
-// TODO: Create function for creating Square by numbers with validation
-// TODO: Create function for creating Square by a string with validation
-
-// Letter returns the column (letter) as a string
-func (s Square) Letter() string {
-	return string(rune('A' - 0 + s.letter))
+// StringByValue creates a Square by letter and number integers
+func StringByValue(let int, num int) (Square, error) {
+	if let < 0 || let > 9 || num < 0 || num > 9 {
+		return Square{}, errors.New("String coordinates out of bounds")
+	}
+	return Square{let, num}, nil
 }
 
-// Number returns the row (number) as a string
-func (s Square) Number() string {
-	return fmt.Sprint(s.number + 1)
+// StringByString creates a Square by a string of the coordinates
+func StringByString(coords string) (Square, error) {
+	// This might not work every time
+	return Square{0, 0}, nil
 }
 
-// Square returns the column and row as a two-letter string
-func (s Square) Square() string {
-	return s.Letter() + s.Number()
+// PrintLetter returns the column (letter) as a string
+func (s Square) PrintLetter() string {
+	return string(rune('A' - 0 + s.Letter))
+}
+
+// PrintNumber returns the row (number) as a string
+func (s Square) PrintNumber() string {
+	return fmt.Sprint(s.Number + 1)
+}
+
+// PrintSquare returns the column and row as a two-letter string
+func (s Square) PrintSquare() string {
+	return s.PrintLetter() + s.PrintNumber()
 }
--- internal/board/square_test.go
+++ internal/board/square_test.go
@@ -10,33 +10,33 @@ var examples = []Square{
 	Square{6, 9},
 }
 
-func TestLetter(t *testing.T) {
+func TestPrintLetter(t *testing.T) {
 	results := [5]string{"A", "C", "F", "J", "G"}
 
 	for i := 0; i < 5; i++ {
-		answer := examples[i].Letter()
+		answer := examples[i].PrintLetter()
 		if answer != results[i] {
 			t.Errorf("Letter function was incorrect, got: %v, want:%v", answer, results[i])
 		}
 	}
 }
 
-func TestNumber(t *testing.T) {
+func TestPrintNumber(t *testing.T) {
 	results := [5]string{"1", "3", "3", "7", "10"}
 
 	for i := 0; i < 5; i++ {
-		answer := examples[i].Number()
+		answer := examples[i].PrintNumber()
 		if answer != results[i] {
 			t.Errorf("Letter function was incorrect, got: %v, want:%v", answer, results[i])
 		}
 	}
 }
 
-func TestSquare(t *testing.T) {
+func TestPrintSquare(t *testing.T) {
 	results := [5]string{"A1", "C3", "F3", "J7", "G10"}
 
 	for i := 0; i < 4; i++ {
-		answer := examples[i].Square()
+		answer := examples[i].PrintSquare()
 		if answer != results[i] {
 			t.Errorf("Square function was incorrect, got: %v, want:%v", answer, results[i])
 		}
