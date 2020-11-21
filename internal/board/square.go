8dfdaca3213099e75d746e9d2cae6bb5c6b4271a
Peter Marks <eaglerock@clutch.eagleworld.net>
2020-11-21 15:18:21 -0500

    Almost done with square routines

--- internal/board/square.go
+++ internal/board/square.go
@@ -3,7 +3,13 @@ package board
 import (
 	"errors"
 	"fmt"
+	"strconv"
+	"strings"
 )
+
+// This is probably a bad idea, but I'm doing it anyway
+var columns = map[string]int{"A": 1, "B": 2, "C": 3, "D": 4, "E": 5,
+	"F": 6, "G": 7, "H": 8, "I": 9, "J": 10}
 
 // Square is a struct for holding a coordinate on the board
 type Square struct {
@@ -21,8 +27,21 @@ func StringByValue(let int, num int) (Square, error) {
 
 // StringByString creates a Square by a string of the coordinates
 func StringByString(coords string) (Square, error) {
-	// This might not work every time
-	return Square{0, 0}, nil
+	chars := strings.Split(coords, "")
+	if len(chars) > 2 {
+		return Square{}, errors.New("Coordinate string has more than two characters")
+	}
+
+	if let, found := columns[chars[0]]; found {
+		num, _ := strconv.Atoi(chars[1])
+		if num < 0 || num > 9 {
+			return Square{}, errors.New("String number coordinate out of bounds")
+		}
+
+		return Square{let, num}, nil
+	}
+
+	return Square{}, errors.New("String letter coordinate out of bounds")
 }
 
 // PrintLetter returns the column (letter) as a string
--- internal/board/square_test.go
+++ internal/board/square_test.go
@@ -2,12 +2,76 @@ package board
 
 import "testing"
 
-var examples = []Square{
+var exampleValues = [5][2]int{
+	{0, 0},
+	{2, 2},
+	{5, 2},
+	{9, 6},
+	{6, 9},
+}
+
+var exampleStrings = [5]string{"A1", "C3", "F3", "J7", "G10"}
+
+var examples = [5]Square{
 	Square{0, 0},
 	Square{2, 2},
 	Square{5, 2},
 	Square{9, 6},
 	Square{6, 9},
+}
+
+var badValues = [5][2]int{
+	{-1, 0},
+	{0, 10},
+	{11, 5},
+	{-1, -1},
+	{12, -12},
+}
+
+var badStrings = [5]string{"Z1", "3A", "AA", "AA1", "B10"}
+
+func TestStringByValue(t *testing.T) {
+	for i, input := range exampleValues {
+		answer, err := StringByValue(input[0], input[1])
+
+		if err != nil {
+			t.Errorf("StringByValue returned an error: %v", err)
+		} else if answer != examples[i] {
+			t.Errorf("StringByValue function was incorrect, got: %v, want:%v", answer, examples[i])
+		}
+	}
+}
+
+func TestBadStringByValue(t *testing.T) {
+	for _, input := range badValues {
+		answer, err := StringByValue(input[0], input[1])
+
+		if err == nil {
+			t.Errorf("StringByValue did not error as expected with %v, returned Square: %v", input, answer)
+		}
+	}
+}
+
+func TestStringByString(t *testing.T) {
+	for i, input := range exampleStrings {
+		answer, err := StringByString(input)
+
+		if err != nil {
+			t.Errorf("StringByString returned an error: %v", err)
+		} else if answer != examples[i] {
+			t.Errorf("StringByString function was incorrect, got: %v, want:%v", answer, examples[i])
+		}
+	}
+}
+
+func TestBadStringByString(t *testing.T) {
+	for _, input := range badStrings {
+		answer, err := StringByString(input)
+
+		if err == nil {
+			t.Errorf("StringByString did not error as expected with %v, returned Square: %v", input, answer)
+		}
+	}
 }
 
 func TestPrintLetter(t *testing.T) {
@@ -33,12 +97,10 @@ func TestPrintNumber(t *testing.T) {
 }
 
 func TestPrintSquare(t *testing.T) {
-	results := [5]string{"A1", "C3", "F3", "J7", "G10"}
-
 	for i := 0; i < 4; i++ {
 		answer := examples[i].PrintSquare()
-		if answer != results[i] {
-			t.Errorf("Square function was incorrect, got: %v, want:%v", answer, results[i])
+		if answer != exampleStrings[i] {
+			t.Errorf("Square function was incorrect, got: %v, want:%v", answer, exampleStrings[i])
 		}
 	}
 }
