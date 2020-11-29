f19934ce3a3e1487fdfa8fea5221743a93055546
Peter Marks <eaglerock@clutch.eagleworld.net>
2020-11-28 23:47:59 -0500

    First pass at board routines

--- internal/board/board.go
+++ internal/board/board.go
@@ -1,1 +1,75 @@
 package board
+
+import "errors"
+
+var values = map[string]int{
+	"Empty": 0, "Miss": 1, "Destroyer": 2, "Submarine": 3,
+	"Cruiser": 4, "Battleship": 5, "Carrier": 6, "Hit": 7,
+}
+
+var status = [8]string{
+	"Empty", "Miss", "Destroyer", "Submarine",
+	"Cruiser", "Battleship", "Carrier", "Hit",
+}
+
+// Board is a type for holding a standard 10x10 Battleship game board.
+type Board [10][10]int
+
+// SetString sets a board value to a given string value
+func (b *Board) SetString(s Square, value string) error {
+	if val, ok := values[value]; ok {
+		b[s.Letter][s.Number] = val
+		return nil
+	}
+	return errors.New("Given value is not a valid value")
+}
+
+// SetInt sets a board value to a given integer value
+func (b *Board) SetInt(s Square, value int) error {
+	if value > 0 && value < 8 {
+		b[s.Letter][s.Number] = value
+		return nil
+	}
+	return errors.New("Given value out of range")
+}
+
+// GetString returns a given Square's string value
+func (b Board) GetString(s Square) string {
+	return status[b[s.Letter][s.Number]]
+}
+
+// GetInt returns a given Square's integer value
+func (b Board) GetInt(s Square) int {
+	return b[s.Letter][s.Number]
+}
+
+// IsEmpty returns whether a given Square is empty
+func (b Board) IsEmpty(s Square) bool {
+	return (b[s.Letter][s.Number] == 0)
+}
+
+// IsMiss returns whether a given Square is a miss
+func (b Board) IsMiss(s Square) bool {
+	return (b[s.Letter][s.Number] == 1)
+}
+
+// IsHit returns whether a given square is any type of hit
+func (b Board) IsHit(s Square) bool {
+	return (b[s.Letter][s.Number] > 1)
+}
+
+// IsUnsunk returns whether a given square is to an unsunk ship
+func (b Board) IsUnsunk(s Square) bool {
+	return (b[s.Letter][s.Number] == 7)
+}
+
+// IsSunk returns whether a given square is to any sunk ship
+func (b Board) IsSunk(s Square) bool {
+	val := b[s.Letter][s.Number]
+	return (val > 1 && val < 7)
+}
+
+// IsPiece returns whether a square belongs to a specific piece
+// func (b Board) IsShip(s Square, p Piece) bool {
+// 	return (status[b[s.Letter][s.Number]] == p.String)
+// }
--- internal/board/board_test.go
+++ internal/board/board_test.go
@@ -1,1 +1,185 @@
 package board
+
+import (
+	"testing"
+)
+
+var boardSquares = [5]Square{
+	{0, 0},
+	{2, 2},
+	{5, 2},
+	{9, 6},
+	{6, 9},
+}
+
+var boardStrings = [5]string{
+	"Miss", "Destroyer", "Battleship", "Carrier", "Hit",
+}
+
+var boardIntegers = [5]int{1, 2, 5, 6, 7}
+
+var badBoardStrs = [5]string{
+	"Mister", "destroyer", "Your mom", "Poop", "Mah boat",
+}
+
+var badBoardInts = [5]int{-1, 8, 20, -3, 17}
+
+var boardTestVals = [5]int{0, 1, 2, 4, 7}
+
+func TestSetString(t *testing.T) {
+	var testboard Board
+
+	for i, input := range boardSquares {
+		err := testboard.SetString(input, boardStrings[i])
+		answer := testboard[input.Letter][input.Number]
+
+		if err != nil {
+			t.Errorf("SetString returned an error: %v", err)
+		} else if answer != boardIntegers[i] {
+			t.Errorf("SetString function was incorrect, got: %v, want:%v", answer, boardIntegers[i])
+		}
+	}
+}
+
+func TestSetInt(t *testing.T) {
+	var testboard Board
+
+	for i, input := range boardSquares {
+		err := testboard.SetInt(input, boardIntegers[i])
+		answer := testboard[input.Letter][input.Number]
+
+		if err != nil {
+			t.Errorf("SetInt returned an error: %v", err)
+		} else if answer != boardIntegers[i] {
+			t.Errorf("SetInt function was incorrect, got: %v, want:%v", answer, boardIntegers[i])
+		}
+	}
+}
+
+func TestBadSetString(t *testing.T) {
+	var testboard Board
+
+	for i, input := range boardSquares {
+		err := testboard.SetString(input, badBoardStrs[i])
+
+		if err == nil {
+			t.Errorf(
+				"SetString did not error as expected with %v, set value: %v",
+				badBoardStrs[i],
+				testboard[input.Letter][input.Number],
+			)
+		}
+	}
+}
+
+func TestBadSetInt(t *testing.T) {
+	var testboard Board
+
+	for i, input := range boardSquares {
+		err := testboard.SetInt(input, badBoardInts[i])
+
+		if err == nil {
+			t.Errorf(
+				"SetInt did not error as expected with %v, set value: %v",
+				badBoardInts[i],
+				testboard[input.Letter][input.Number],
+			)
+		}
+	}
+}
+
+func TestGetString(t *testing.T) {
+	var testboard Board
+
+	for i, input := range boardSquares {
+		testboard.SetInt(input, boardIntegers[i])
+		answer := testboard.GetString(input)
+
+		if answer != boardStrings[i] {
+			t.Errorf("GetString function was incorrect, got: %v, want:%v", answer, boardStrings[i])
+		}
+	}
+}
+
+func TestGetInt(t *testing.T) {
+	var testboard Board
+
+	for i, input := range boardSquares {
+		testboard.SetInt(input, boardIntegers[i])
+		answer := testboard.GetInt(input)
+
+		if answer != boardIntegers[i] {
+			t.Errorf("GetInt function was incorrect, got: %v, want:%v", answer, boardIntegers[i])
+		}
+	}
+}
+
+func TestIsEmpty(t *testing.T) {
+	var testboard Board
+	var expected = [5]bool{true, false, false, false, false}
+
+	for i, input := range boardSquares {
+		testboard.SetInt(input, boardTestVals[i])
+		answer := testboard.IsEmpty(input)
+
+		if answer != expected[i] {
+			t.Errorf("IsEmpty function was incorrect, got: %v, want:%v", answer, expected[i])
+		}
+	}
+}
+
+func TestIsMiss(t *testing.T) {
+	var testboard Board
+	var expected = [5]bool{false, true, false, false, false}
+
+	for i, input := range boardSquares {
+		testboard.SetInt(input, boardTestVals[i])
+		answer := testboard.IsMiss(input)
+
+		if answer != expected[i] {
+			t.Errorf("IsMiss function was incorrect, got: %v, want:%v", answer, expected[i])
+		}
+	}
+}
+
+func TestIsHit(t *testing.T) {
+	var testboard Board
+	var expected = [5]bool{false, false, true, true, true}
+
+	for i, input := range boardSquares {
+		testboard.SetInt(input, boardTestVals[i])
+		answer := testboard.IsHit(input)
+
+		if answer != expected[i] {
+			t.Errorf("IsHit function was incorrect, got: %v, want:%v", answer, expected[i])
+		}
+	}
+}
+
+func TestIsUnsunk(t *testing.T) {
+	var testboard Board
+	var expected = [5]bool{false, false, false, false, true}
+
+	for i, input := range boardSquares {
+		testboard.SetInt(input, boardTestVals[i])
+		answer := testboard.IsUnsunk(input)
+
+		if answer != expected[i] {
+			t.Errorf("IsUnsunk function was incorrect, got: %v, want:%v", answer, expected[i])
+		}
+	}
+}
+
+func TestIsSunk(t *testing.T) {
+	var testboard Board
+	var expected = [5]bool{false, false, true, true, false}
+
+	for i, input := range boardSquares {
+		testboard.SetInt(input, boardTestVals[i])
+		answer := testboard.IsSunk(input)
+
+		if answer != expected[i] {
+			t.Errorf("IsSunk function was incorrect, got: %v, want:%v", answer, expected[i])
+		}
+	}
+}
