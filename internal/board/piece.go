8772c5798c072b3c5270d9abf2f1f2e012ea7223
Peter Marks <peter.marks@gmail.com>
2020-12-05 11:17:37 -0500

    complete Ship type, functions, and tests

--- internal/board/piece.go
+++ internal/board/piece.go
@@ -15,21 +15,44 @@ type Ship string
 
 // Piece is a struct for defining a piece and its position
 type Piece struct {
-	Type   string
+	Type   Ship
 	Coords []Square
 }
 
-// PieceByVector defines a Piece by the following variables:
-// shipType: a string with the desired ship type
-// startSquare: the starting Square of the ship from the topmost, leftmost square
-// horizontal: a boolean that determines either horizontal or vertical placement
-func PieceByVector(shipType string, startSquare Square, horizontal bool) (Piece, error) {
-	if squares, ok := ships[shipType]; ok {
-		// TODO: do piece on board validation here
-		var newPiece Piece
-		newPiece.Type = shipType
-		newPiece.Coords = make([]Square, squares)
-		return newPiece, nil
+// NewShip will return a Ship type after input validation
+func NewShip(shipType string) (Ship, error) {
+	if _, ok := ships[shipType]; ok {
+		return Ship(shipType), nil
 	}
-	return Piece{}, errors.New("Something went wrong")
+	return Ship(""), errors.New("A valid Ship type was not given")
+}
+
+// Type will return the type of ship as a string
+func (s Ship) Type() string {
+	return string(s)
+}
+
+// Length will return the length of the ship as an integer
+func (s Ship) Length() int {
+	return ships[string(s)]
+}
+
+// ShipTypes will return a slice of all 5 Ship types
+func ShipTypes() []Ship {
+	shipTypes := make([]Ship, 0, 5)
+	for ship := range ships {
+		shipTypes = append(shipTypes, Ship(ship))
+	}
+	return shipTypes
+}
+
+// NewPiece defines a Piece by the following variables:
+func NewPiece(shipType Ship, startSquare Square, horizontal bool) (Piece, error) {
+	// TODO: do piece on board validation here
+	var newPiece Piece
+	newPiece.Type = shipType
+	newPiece.Coords = make([]Square, shipType.Length())
+	return newPiece, nil
+
+	// return Piece{}, errors.New("Something went wrong")
 }
--- internal/board/piece_test.go
+++ internal/board/piece_test.go
@@ -1,1 +1,82 @@
 package board
+
+import (
+	"testing"
+)
+
+var exampleTypes = [5]string{
+	"Carrier",
+	"Battleship",
+	"Cruiser",
+	"Submarine",
+	"Destroyer",
+}
+
+var exampleShips = [5]Ship{
+	Ship("Carrier"),
+	Ship("Battleship"),
+	Ship("Cruiser"),
+	Ship("Submarine"),
+	Ship("Destroyer"),
+}
+
+var exampleSizes = [5]int{5, 4, 3, 3, 2}
+
+var badShips = [5]string{
+	"Your Mom",
+	"destroyer",
+	"BATTLESHIP",
+	"SuBmArInE",
+	"I'm on a boat",
+}
+
+func TestNewShip(t *testing.T) {
+	for i, input := range exampleTypes {
+		answer, err := NewShip(input)
+
+		if err != nil {
+			t.Errorf("NewShip returned an error: %v", err)
+		} else if answer != exampleShips[i] {
+			t.Errorf("NewShip function was incorrect, got: %v, want:%v", answer, exampleShips[i])
+		}
+	}
+}
+
+func TestBadNewShip(t *testing.T) {
+	for _, input := range badShips {
+		answer, err := NewShip(input)
+
+		if err == nil {
+			t.Errorf("NewShip did not error as expected with %v, returned Ship: %v", input, answer)
+		}
+	}
+}
+
+func TestShipType(t *testing.T) {
+	for i, input := range exampleShips {
+		answer := input.Type()
+
+		if answer != exampleTypes[i] {
+			t.Errorf("Type was incorrect, got: %v, want: %v", answer, exampleStrings[i])
+		}
+	}
+}
+
+func TestShipLength(t *testing.T) {
+	for i, input := range exampleShips {
+		answer := input.Length()
+
+		if answer != exampleSizes[i] {
+			t.Errorf("Length was incorrect, got %v, want: %v", answer, exampleSizes[i])
+		}
+	}
+}
+
+func TestShipTypes(t *testing.T) {
+	answer := ShipTypes()
+	for i, input := range answer {
+		if input != exampleShips[i] {
+			t.Errorf("ShipType was incorrect, got: %v, want: %v", input, exampleShips[i])
+		}
+	}
+}
