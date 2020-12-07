8a6f26131f9629d0b65c14232826e5ffbf72f8d1
Peter Marks <peter.marks@gmail.com>
2020-12-06 21:58:23 -0500

    wrote NewPiece() unit test

--- internal/board/piece_test.go
+++ internal/board/piece_test.go
@@ -30,6 +30,22 @@ var badShips = [5]string{
 	"I'm on a boat",
 }
 
+var pieceSquares = [5]Square{
+	{0, 0},
+	{2, 2},
+	{5, 2},
+	{9, 6},
+	{6, 9},
+}
+
+var examplePieces = [5]Piece{
+	{Ship("Carrier"), []Square{{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}}},
+	{Ship("Battleship"), []Square{{2, 2}, {2, 3}, {2, 4}, {2, 5}}},
+	{Ship("Cruiser"), []Square{{5, 2}, {6, 2}, {7, 2}}},
+	{Ship("Submarine"), []Square{{9, 6}, {9, 7}, {9, 8}}},
+	{Ship("Destroyer"), []Square{{6, 9}, {7, 9}}},
+}
+
 func TestNewShip(t *testing.T) {
 	for i, input := range exampleTypes {
 		answer, err := NewShip(input)
@@ -37,7 +53,7 @@ func TestNewShip(t *testing.T) {
 		if err != nil {
 			t.Errorf("NewShip returned an error: %v", err)
 		} else if answer != exampleShips[i] {
-			t.Errorf("NewShip function was incorrect, got: %v, want:%v", answer, exampleShips[i])
+			t.Errorf("NewShip function was incorrect, got: %v, want: %v", answer, exampleShips[i])
 		}
 	}
 }
@@ -77,6 +93,27 @@ func TestShipTypes(t *testing.T) {
 	for i, input := range answer {
 		if input != exampleShips[i] {
 			t.Errorf("ShipType was incorrect, got: %v, want: %v", input, exampleShips[i])
+		}
+	}
+}
+
+func TestNewPiece(t *testing.T) {
+	horizontal := [5]bool{true, false, true, false, true}
+
+	for i, input := range exampleShips {
+		answer, err := NewPiece(input, pieceSquares[i], horizontal[i])
+
+		if err != nil {
+			t.Errorf("NewPiece returned an error: %v", err)
+		} else {
+			if answer.Type != examplePieces[i].Type {
+				t.Errorf("NewPiece set type incorrect, got: %v, want: %v", answer.Type, examplePieces[i].Type)
+			}
+			for j, coord := range answer.Coords {
+				if coord != examplePieces[i].Coords[j] {
+					t.Errorf("NewPiece coordinates were incorrect, got: %v, want: %v", coord, examplePieces[i].Coords[j])
+				}
+			}
 		}
 	}
 }
