ff0adce721345a81736123ee82c2ca6d81d84dd0
Peter Marks <peter.marks@gmail.com>
2025-07-18 23:40:09 -0400

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "gobat - Go 1.21.1 dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs, ... }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+
+    go = pkgs.go_1_21;  # Go 1.21.x series
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [ go ];
+
+      shellHook = ''
+        export GOCACHE=$PWD/.cache/go-build
+        export GOPATH=$PWD/.gopath
+        export GOMODCACHE=$PWD/.gopath/pkg/mod
+        export PATH=$GOPATH/bin:$PATH
+
+        echo "🦫 Go dev shell ready (Go version: $(go version))"
+      '';
+    };
+  };
+}
